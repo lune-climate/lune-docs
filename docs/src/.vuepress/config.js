c85f9a5b8a21f73e4601998f1603605c1cb85f77
Roberto Bruggemann <1833249+rbruggem@users.noreply.github.com>
2022-03-04 09:21:03 +0000

    Update favicon to new version (#49)
    This makes the docs consistent with the website.

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -16,7 +16,7 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
-    ['link', { rel: 'icon', href: 'https://assets.lune.co/lune-logo-dark.png' }],
+    ['link', { rel: 'icon', href: '/favicon.png' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
