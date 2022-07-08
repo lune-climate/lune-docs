cdac265e9163ca522ad78fcdd9faf9561868acf3
Roberto Bruggemann <roberto@lune.co>
2022-07-08 10:41:29 +0100

    Fix data-domain

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -20,7 +20,7 @@ module.exports = {
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['script', { defer: true, dataDomain: 'docs.lune.co', src: 'https://plausible.io/js/plausible.js' }]
+    ['script', { defer: true, 'data-domain': 'docs.lune.co', src: 'https://plausible.io/js/plausible.js' }]
   ],
 
   /**
