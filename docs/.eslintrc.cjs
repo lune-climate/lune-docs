85feb6a14ff01436532f303e59de2e222444d7bb
Ruben Aguiar <r.aguiar9080@gmail.com>
2022-07-28 13:51:40 +0100

    Improve dev setup
    Add eslint, prettier, unused-imports functionalities.
    
    Signed-off-by: Ruben Aguiar <r.aguiar9080@gmail.com>

--- docs/.eslintrc.cjs
+++ docs/.eslintrc.cjs
@@ -1,53 +1,46 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es2021: true,
     },
-    "extends": [
-        "standard",
-        "prettier",
-        "eslint:recommended",
-        "plugin:jest/recommended",
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
+    extends: ['standard', 'prettier', 'eslint:recommended'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
     },
-    "plugins": [
-        "@typescript-eslint",
-        "jest",
-        "simple-import-sort"
-    ],
-    "ignorePatterns": ["v1Types.ts"],
+    plugins: ['@typescript-eslint', 'simple-import-sort'],
+    ignorePatterns: ['v1Types.ts'],
     rules: {
-      'comma-dangle': ['error', 'always-multiline'],
-      'space-before-function-paren': ['error', {
-          'anonymous': 'never',
-          'named': 'never',
-          'asyncArrow': 'always'
-      }],
-      'no-useless-constructor': 'off',
-      'no-unused-vars': 'off',
-      'no-extra-semi': 'off',
-      '@typescript-eslint/no-useless-constructor': ['error'],
-      '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_', 'varsIgnorePattern': '^_' }],
-      'simple-import-sort/imports': 'error',
-      'complexity': ['error', { 'max': 14 }],
-      'jest/expect-expect': 'off',
-      'jest/no-conditional-expect': 'off',
+        'comma-dangle': ['error', 'always-multiline'],
+        'space-before-function-paren': [
+            'error',
+            {
+                anonymous: 'never',
+                named: 'never',
+                asyncArrow: 'always',
+            },
+        ],
+        'no-useless-constructor': 'off',
+        'no-unused-vars': 'off',
+        'no-extra-semi': 'off',
+        '@typescript-eslint/no-useless-constructor': ['error'],
+        '@typescript-eslint/no-unused-vars': [
+            'error',
+            { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+        ],
+        'simple-import-sort/imports': 'error',
+        complexity: ['error', { max: 14 }],
 
-      // To support TS overloads
-      // https://github.com/typescript-eslint/typescript-eslint/blob/6fd476c32c4757cb9f4c442f0cd92875671eed30/packages/eslint-plugin/docs/rules/no-redeclare.md
-      'no-redeclare': 'off',
-      '@typescript-eslint/no-redeclare': ['error'],
+        // To support TS overloads
+        // https://github.com/typescript-eslint/typescript-eslint/blob/6fd476c32c4757cb9f4c442f0cd92875671eed30/packages/eslint-plugin/docs/rules/no-redeclare.md
+        'no-redeclare': 'off',
+        '@typescript-eslint/no-redeclare': ['error'],
     },
-    overrides: [{
-      files: [
-        'src/*.test.ts'
-      ],
-      env: {
-          jest: true
-      }
-    }],
-};
+    overrides: [
+        {
+            files: ['src/*.test.ts'],
+            env: {},
+        },
+    ],
+}
