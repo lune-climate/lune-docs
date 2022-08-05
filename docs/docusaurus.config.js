94bc897f006b28022dfce91032d56684c6ecf0e5
Ruben Aguiar <r.aguiar9080@gmail.com>
2022-08-05 14:10:42 +0100

    Improve config file
    Mostly clean things up. Add some configs and comments. Make it more the
    design we're aiming towards.
    
    Signed-off-by: Ruben Aguiar <r.aguiar9080@gmail.com>

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -14,11 +14,6 @@ const config = {
     onBrokenMarkdownLinks: 'warn',
     favicon: 'img/favicon.png',
 
-    // GitHub pages deployment config.
-    // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'lune-climate', // Usually your GitHub org/user name.
-    projectName: 'lune-prototype-docusaurus', // Usually your repo name.
-
     // Even if you don't use internalization, you can use this field to set useful
     // metadata like html lang. For example, if your site is Chinese, you may want
     // to replace "en" with "zh-Hans".
@@ -51,6 +46,22 @@ const config = {
                 disableSwitch: false,
                 respectPrefersColorScheme: true,
             },
+            // meta tag, in particular og:image and twitter:image
+            image: 'img/lune-icon-512.png',
+
+            // HTML metadata (and override existing ones).
+            // metadata: [{name: 'twitter:card', content: 'summary'}],
+
+            // Announcement bar. This is a non-fixed optionally dismisable panel above navbar.
+            // announcementBar: {
+            //    id: 'support_us',
+            //    content:
+            //      'We are looking to revamp our docs, please fill <a target="_blank" rel="noopener noreferrer" href="#">this survey</a>',
+            //    backgroundColor: '#fafbfc',
+            //    textColor: '#091E42',
+            //    isCloseable: false,
+            //  },
+
             navbar: {
                 title: 'Documentation',
                 logo: {
@@ -60,31 +71,38 @@ const config = {
                 },
                 items: [
                     {
-                        href: 'https://github.com/lune-climate/lune-docs',
-                        label: 'GitHub',
+                        type: 'search',
                         position: 'right',
                     },
                 ],
+                hideOnScroll: true,
             },
+
             footer: {
                 style: 'light',
+                logo: {
+                    alt: 'Lune Logo',
+                    src: 'img/lune-logo-512.png',
+                    srcDark: 'img/lune-logo-512-white.png',
+                    width: 80,
+                },
                 links: [
                     {
-                        title: 'Useful Links',
-                        items: [
-                            {
-                                label: 'Twitter',
-                                href: 'https://twitter.com/luneclimate',
-                            },
-                            {
-                                label: 'Blog',
-                                to: 'https://lune.co/blog/',
-                            },
-                        ],
+                        label: 'Blog',
+                        to: 'https://lune.co/blog/',
+                    },
+                    {
+                        label: 'Twitter',
+                        to: 'https://twitter.com/luneclimate',
+                    },
+                    {
+                        label: 'GitHub',
+                        to: 'https://github.com/lune-climate/lune-docs',
                     },
                 ],
                 copyright: `Copyright © ${new Date().getFullYear()} Lune Climate Ltd. All rights reserved.`,
             },
+
             prism: {
                 theme: lightCodeTheme,
                 darkTheme: darkCodeTheme,
