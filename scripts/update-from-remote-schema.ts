lune-climate/lune-docs

commit d0771b905d9657dcbfca6f962ecef0c69c572542

Resources: align descriptions with snippets (#422)
Descriptions and Snippets are now consistent.

Also, move page title inside the first "block" for proper alignment.

scripts/update-from-remote-schema.ts
@@ -38,14 +38,13 @@ function createResourceMDX(data: any, label: string, sidebarPosition?: number): 
             ? `---
 sidebar_position: 1
 sidebar_label: ${label}
+hide_title: true
 ---`
             : ''
     }
-# ${to.sentence(data.component)}
-
 import Resource from '@site/src/components/Resource';
 
-<Resource json={${JSON.stringify(data)}}/>`
+<Resource name={"${to.sentence(data.component)}"} json={${JSON.stringify(data)}}/>`
 }
 
 function createContextAPISchema(schema: any): string {

src/components/Resource/index.tsx
@@ -11,7 +11,7 @@ import {
 } from 'lune-ui-lib'
 import React from 'react'
 
-export default function ResourceParser(props: { json: any }): JSX.Element {
+export default function ResourceParser(props: { name: string; json: any }): JSX.Element {
     // remove last element because endpoints listed in this page are at the same level
     const hrefPrefix = useRelativePathPrefix().split('/').slice(0, -1).join('/')
 
@@ -49,62 +49,61 @@ export default function ResourceParser(props: { json: any }): JSX.Element {
 
     return (
         <section className="page">
-            {props.json.description && (
-                <div className="body3 pageDescription">{props.json.description}</div>
-            )}
             <ApiReferenceSection>
+                <>
+                    <h1>{props.name}</h1>
+                    <div className="body3 pageDescription">{props.json.description}</div>
+                </>
+                {props.json.endpoints && (
+                    <Snippet header="Endpoints" sx={{ marginBottom: '16px' }}>
+                        <SnippetItem>
+                            <div className="endpointsTable">
+                                {props.json.endpoints.map((endpoint, i) => (
+                                    <div
+                                        key={i}
+                                        className="row"
+                                        style={{
+                                            border: 'solid 1px black',
+                                        }}
+                                    >
+                                        <a
+                                            key={i}
+                                            href={`${hrefPrefix}/${formatPath(
+                                                endpoint.operationId,
+                                            )}`}
+                                        >
+                                            <div
+                                                className="cell"
+                                                style={{
+                                                    textAlign: 'right',
+                                                    width: '50px',
+                                                }}
+                                            >
+                                                {endpoint.method.toUpperCase()}
+                                            </div>
+                                            <div
+                                                className="cell"
+                                                style={{
+                                                    textAlign: 'left',
+                                                }}
+                                            >
+                                                &nbsp;{endpoint.endpoint}
+                                            </div>
+                                        </a>
+                                    </div>
+                                ))}
+                            </div>
+                        </SnippetItem>
+                    </Snippet>
+                )}
+            </ApiReferenceSection>
+            <ApiReferenceSection style={{ marginTop: '32px' }}>
                 <JsonObjectTable>
                     {resourceProperties.map((property) => {
                         return <JsonProperty json={property} topLevelDividers />
                     })}
                 </JsonObjectTable>
-                <>
-                    {props.json.endpoints && (
-                        <Snippet header="Endpoints" sx={{ marginBottom: '16px' }}>
-                            <SnippetItem>
-                                <div className="endpointsTable">
-                                    {props.json.endpoints.map((endpoint, i) => (
-                                        <div
-                                            key={i}
-                                            className="row"
-                                            style={{
-                                                border: 'solid 1px black',
-                                            }}
-                                        >
-                                            <a
-                                                key={i}
-                                                href={`${hrefPrefix}/${formatPath(
-                                                    endpoint.operationId,
-                                                )}`}
-                                            >
-                                                <div
-                                                    className="cell"
-                                                    style={{
-                                                        textAlign: 'right',
-                                                        width: '50px',
-                                                    }}
-                                                >
-                                                    {endpoint.method.toUpperCase()}
-                                                </div>
-                                                <div
-                                                    className="cell"
-                                                    style={{
-                                                        textAlign: 'left',
-                                                    }}
-                                                >
-                                                    &nbsp;{endpoint.endpoint}
-                                                </div>
-                                            </a>
-                                        </div>
-                                    ))}
-                                </div>
-                            </SnippetItem>
-                        </Snippet>
-                    )}
-                    <Snippet header={props.json.component || props.json.name}>
-                        {exampleSnippet}
-                    </Snippet>
-                </>
+                <Snippet header={props.json.component || props.json.name}>{exampleSnippet}</Snippet>
             </ApiReferenceSection>
         </section>
     )

commit 29709b79d593141f3b48bada9d2ff49c1d420b22

Ensure page title is correct
This change ensures:
1. the correct title is used
2. in the correct format
in the browser tab

The regression was accidentally introduced in
https://github.com/lune-climate/lune-docs/pull/422.

scripts/update-from-remote-schema.ts
@@ -33,18 +33,20 @@ import Endpoint from '@site/src/components/Endpoint';
 }
 
 function createResourceMDX(data: any, label: string, sidebarPosition?: number): string {
+    const name = to.sentence(data.component)
     return `${
         sidebarPosition
             ? `---
 sidebar_position: 1
 sidebar_label: ${label}
 hide_title: true
+title: ${name}
 ---`
             : ''
     }
 import Resource from '@site/src/components/Resource';
 
-<Resource name={"${to.sentence(data.component)}"} json={${JSON.stringify(data)}}/>`
+<Resource name={"${name}"} json={${JSON.stringify(data)}}/>`
 }
 
 function createContextAPISchema(schema: any): string {